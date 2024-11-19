23add6f2a821d8c7fb6c42cc5ab56c52f63a0c44
Viswa <viswa@c.com>
2024-11-19 14:23:50 +0530

    Auth Signed-off-by: Viswa <viswa@c.com>

---
+++ src/main/java/com/booking/app/flows/Authenticate.java
@@ -0,0 +1,19 @@
+package com.booking.app.flows;
+
+import com.booking.app.pojo.Auth;
+import com.framework.core.api.restclient.RequestParam;
+import com.framework.core.api.restclient.ResponseFetcher;
+import com.framework.core.utils.properties.PropertiesUtils;
+
+public class Authenticate {
+	public static ResponseFetcher createBooking(PropertiesUtils properties){
+        RequestParam request;
+        request = new RequestParam(properties.getProperty("baseUri"),properties.getProperty("authBasePath"));
+        
+        Auth auth = new Auth(properties.getProperty("authUname"), properties.getProperty("authPwd"));
+
+        request.setRequestHeaders("Content-Type","application/json");
+        request.setRequestBody(auth);
+        return request.createRequest().post();
+    }
+}
